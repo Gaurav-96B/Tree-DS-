d41d2f57ad948ff4b32ec190fcd093e5a4caa9d8
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-25 21:44:14 +0530

    Create Vertical traversal.java

---
+++ Vertical Order Traversal/Vertical traversal.java
@@ -0,0 +1,58 @@
+class Pair
+{
+    Node node;
+    int hd;
+    Pair(Node node,int hd)
+    {
+       this.node=node;
+       this.hd=hd;
+    }
+}
+class BinaryTree
+{
+    static ArrayList <Integer> verticalOrder(Node root)
+    {
+        Queue<Pair> q=new LinkedList<>();
+        Map<Integer,ArrayList<Integer>> m=new TreeMap<>();
+        q.add(new Pair(root,0));
+        while(!q.isEmpty())
+        {
+            int z=q.size();
+            while(z>0)
+            {
+            Pair p=q.poll();
+            int hd=p.hd;
+            Node curr=p.node;
+            if(m.containsKey(hd))
+            {
+            m.get(hd).add(curr.data);
+            }
+            else
+            {
+                ArrayList<Integer> al=new ArrayList<>();
+                al.add(curr.data);
+                m.put(hd,al);
+            }
+            if(curr.left!=null)
+            {
+                q.add(new Pair(curr.left,hd-1));
+            }
+            if(curr.right!=null)
+            {
+                q.add(new Pair(curr.right,hd+1));
+            }
+            z--;
+        }
+        }
+        ArrayList<Integer>a=new ArrayList<>();
+        for(Map.Entry<Integer,ArrayList<Integer>> me:m.entrySet()){
+             ArrayList<Integer> al=me.getValue();
+             for(int v:al)
+             a.add(v);
+        }
+    
+    return a;
+    
+    }
+        
+    }
