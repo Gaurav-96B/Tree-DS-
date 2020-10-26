2e778d05cc5c0861bb9f338c6036174eb6ebf8bf
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-26 12:24:22 +0530

    Create Symmetric tree.java

---
+++ Tree Recursion/Symmetric tree.java
@@ -0,0 +1,24 @@
+class GfG
+{
+     public static boolean symmetric(Node n1,Node n2)
+   {
+       if(n1==null&&n2==null)
+       {
+           return true;
+       }
+       if(n1==null||n2==null)
+       {
+           return false;
+       }
+       return (n1.data==n2.data)&&symmetric(n1.left,n2.right)&&symmetric(n1.right,n2.left);
+   }
+    public static boolean isSymmetric(Node root)
+    {
+       if(root==null) 
+       {
+          return true; 
+       }
+       return symmetric(root.left,root.right);
+    }
+    
+}
