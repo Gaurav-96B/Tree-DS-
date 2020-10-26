fcad9c5908c067f3db498bd6148cee3da693c18d
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-26 12:17:21 +0530

    Create DiagonalSum.java

---
+++ Tree Recursion/DiagonalSum.java
@@ -0,0 +1,59 @@
+class Pair
+{
+    Node node;
+    int hd;
+    Pair(Node node,int hd)
+    {
+        this.node=node;
+        this.hd=hd;
+    }
+}
+class Tree {
+    public static ArrayList <Integer> diagonalSum(Node root) 
+    {
+        Queue<Pair> q=new LinkedList<>();
+        Map<Integer,ArrayList<Integer>> m=new TreeMap<>();
+        q.add(new Pair(root,0));
+        while(!q.isEmpty())
+        {
+            int z=q.size();
+            while(z>0)
+            {
+            Pair p=q.poll();
+            int hd=p.hd;
+            Node curr=p.node;
+            if(m.containsKey(hd))
+            {
+            m.get(hd).add(curr.data);
+            }
+            else
+            {
+                ArrayList<Integer> al=new ArrayList<>();
+                al.add(curr.data);
+                m.put(hd,al);
+            }
+            if(curr.left!=null)
+            {
+                q.add(new Pair(curr.left,hd+11));
+            }
+            if(curr.right!=null)
+            {
+                q.add(new Pair(curr.right,hd));
+            }
+            z--;
+        }
+        }
+        ArrayList<Integer>a=new ArrayList<>();
+        for(Map.Entry<Integer,ArrayList<Integer>> me:m.entrySet()){
+            int sum=0;
+             ArrayList<Integer> al=me.getValue();
+             for(int v:al)
+             {
+                 sum=sum+v;
+             }
+             a.add(sum);
+        }
+    
+    return a;
+    }
+}
