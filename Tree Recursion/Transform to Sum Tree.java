4cf85bc8c11cb80a4944d75a50601db1ca6974cb
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-02-13 14:54:10 +0530

    Create Transform to Sum Tree.java

---
+++ Tree Recursion/Transform to Sum Tree.java
@@ -0,0 +1,31 @@
+class Tree{
+ public void toSumTree(Node root){
+     if(root==null)
+{
+    return;
+}
+if(root.left==null&&root.right==null)
+{
+    root.data=0;
+}
+else
+{
+    int leftsum=0;
+    int rightsum=0;
+    if(root.left!=null)
+    {
+      leftsum=root.left.data;
+      toSumTree(root.left);
+      leftsum=leftsum+root.left.data;
+    }
+    if(root.right!=null)
+    {
+      rightsum=root.right.data;
+      toSumTree(root.right);
+      rightsum=rightsum+root.right.data;
+    }
+    root.data=leftsum+rightsum;
+    
+}
+}   
+}
