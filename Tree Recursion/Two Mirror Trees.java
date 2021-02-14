1ccb408d334100dd6a478237c6ebbda3181d4d84
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-02-14 18:38:11 +0530

    Create Two Mirror Trees.java

---
+++ Tree Recursion/Two Mirror Trees.java
@@ -0,0 +1,15 @@
+class Tree
+{
+    boolean areMirror(Node a, Node b) 
+    {
+       if(a==null&&b==null)
+        {
+            return true;
+        }
+        if(a==null||b==null)
+        {
+            return false;
+        }
+        return a.data==b.data&&areMirror(a.left,b.right)&&areMirror(a.right,b.left); 
+    }
+}
